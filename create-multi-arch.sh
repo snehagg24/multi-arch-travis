e0106ad313decc821c194410a7130bc6b6c87b44
Rafael Sene <rpsene@gmail.com>
2020-08-11 15:14:55 -0300

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-sudo > /etc/docker/daemon.json
-sudo echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry.docker.io"], "mtu": 1460, "experimental": true}' >> /etc/docker/daemon.json
-sudo cat /etc/docker/daemon.json
-sudo systemctl restart docker
+> /etc/docker/daemon.json
 
+echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry.docker.io"], "mtu": 1460, "experimental": true}' >> /etc/docker/daemon.json
 
-sudo docker manifest create travis/multi-arch-image:latest \
+cat /etc/docker/daemon.json
+
+systemctl restart docker
+
+docker info
+
+docker manifest create travis/multi-arch-image:latest \
 quay.io/rpsene/multi-arch-travis:x86_64 quay.io/rpsene/multi-arch-travis:ppc64le
 
-sudo docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
+#sudo docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
 
-sudo docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
+#sudo docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
 
-sudo docker login quay.io -u "$USER" -p $TOKEN
+#sudo docker login quay.io -u "$USER" -p $TOKEN
 
-sudo docker manifest push quay.io/rpsene/multi-arch-travis:latest
+#sudo docker manifest push quay.io/rpsene/multi-arch-travis:latest
