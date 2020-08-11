12a41d076dce87c771906ef69e77071f6421c023
Rafael Sene <rpsene@gmail.com>
2020-08-11 15:10:57 -0300

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -5,13 +5,14 @@ sudo echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry
 sudo cat /etc/docker/daemon.json
 sudo systemctl restart docker
 
-docker manifest create travis/multi-arch-image:latest \
+
+sudo docker manifest create travis/multi-arch-image:latest \
 quay.io/rpsene/multi-arch-travis:x86_64 quay.io/rpsene/multi-arch-travis:ppc64le
 
-docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
+sudo docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
 
-docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
+sudo docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
 
-docker login quay.io -u "$USER" -p $TOKEN
+sudo docker login quay.io -u "$USER" -p $TOKEN
 
-docker manifest push quay.io/rpsene/multi-arch-travis:latest
+sudo docker manifest push quay.io/rpsene/multi-arch-travis:latest
