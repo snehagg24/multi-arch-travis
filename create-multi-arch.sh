860857114c955764e35b86e493c5a175387699b3
Rafael Sene <rpsene@gmail.com>
2020-08-11 15:28:23 -0300

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -7,8 +7,10 @@ quay.io/rpsene/multi-arch-travis:x86_64 quay.io/rpsene/multi-arch-travis:ppc64le
 
 docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
 
-docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
+docker manifest inspect travis/multi-arch-image
 
 docker login quay.io -u "$USER" -p $TOKEN
+
+docker images
 
 docker manifest push quay.io/rpsene/multi-arch-travis:latest
