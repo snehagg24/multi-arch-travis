a9f9c73a8e30c249c6f282a9a58d3da6d95a4138
Rafael Sene <rpsene@gmail.com>
2020-08-11 14:42:08 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
 docker login quay.io -u "$USER" -p $TOKEN
 docker push quay.io/rpsene/multi-arch-travis:$ARCH
 > /etc/docker/daemon.json
-sudo echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry.docker.io"], "mtu": 1460}' >> /etc/docker/daemon.json
+sudo echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry.docker.io"], "mtu": 1460, "experimental": true}' >> /etc/docker/daemon.json
 cat /etc/docker/daemon.json
