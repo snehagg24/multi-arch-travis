efb1b36c5feaf764229cc516a1f1ae7d0f05a6d0
Rafael Sene <rpsene@gmail.com>
2020-08-11 13:51:03 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
+docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
+docker login quay.io -u "$USER" -p $TOKEN
+docker push quay.io/rpsene/multi-arch-travis:$ARCH
