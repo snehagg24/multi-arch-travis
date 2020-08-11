cbabf09e20d64a14ea760b84536b0d5e9de72f19
Rafael Sene <rpsene@gmail.com>
2020-08-11 14:38:12 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
 docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
 docker login quay.io -u "$USER" -p $TOKEN
 docker push quay.io/rpsene/multi-arch-travis:$ARCH
+sudo sed '/^",experimental": true=.*/a }' /etc/docker/daemon.json
 cat /etc/docker/daemon.json
