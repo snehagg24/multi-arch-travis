6fa8b9b5399489efa83092300b514529b2d78db9
Rafael Sene <rpsene@gmail.com>
2020-08-11 15:27:09 -0300

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -2,15 +2,13 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker buildx version
-
 docker manifest create travis/multi-arch-image:latest \
 quay.io/rpsene/multi-arch-travis:x86_64 quay.io/rpsene/multi-arch-travis:ppc64le
 
-#sudo docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
+docker tag travis/multi-arch-image quay.io/rpsene/multi-arch-travis:latest
 
-#sudo docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
+docker manifest inspect quay.io/rpsene/multi-arch-travis:latest
 
-#sudo docker login quay.io -u "$USER" -p $TOKEN
+docker login quay.io -u "$USER" -p $TOKEN
 
-#sudo docker manifest push quay.io/rpsene/multi-arch-travis:latest
+docker manifest push quay.io/rpsene/multi-arch-travis:latest
