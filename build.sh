515e8b2a3fdcb2acfc4a4d3eae3cb688cfd79a13
Rafael Sene <rpsene@gmail.com>
2020-08-11 14:47:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ docker push quay.io/rpsene/multi-arch-travis:$ARCH
 > /etc/docker/daemon.json
 sudo echo '{"storage-driver": "overlay2", "registry-mirrors": ["https://registry.docker.io"], "mtu": 1460, "experimental": true}' >> /etc/docker/daemon.json
 cat /etc/docker/daemon.json
+sudo systemctl restart docker
+sudo docker version
