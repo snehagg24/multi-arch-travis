43cc571bee4d00cd0c5af2b1ea864eea650c0f80
Rafael Sene <rpsene@gmail.com>
2020-08-18 10:15:20 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 ARCH=$(uname -m)
 
-docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
+ltrace docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
 docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 docker push quay.io/rpsene/multi-arch-travis:$ARCH
