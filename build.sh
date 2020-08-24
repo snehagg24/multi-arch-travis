e514fb9def880ea04b3f0f6bc58b3655b6795e16
snehakpersistent <sneha_kanekar@persistent.com>
2020-08-24 11:56:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 ARCH=$(uname -m)
 
 LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
-docker tag container/$ARCH quay.io/rpsene/multi-arch-travis:$ARCH
+docker tag container/$ARCH quay.io/snehakpersistent/multi-arch-travis:$ARCH
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
-docker push quay.io/rpsene/multi-arch-travis:$ARCH
+docker push quay.io/snehakpersistent/multi-arch-travis:$ARCH
