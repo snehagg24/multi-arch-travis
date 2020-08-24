28c0f32ad3c80d97503890357e7bde26facfa51d
snehakpersistent <sneha_kanekar@persistent.com>
2020-08-24 15:55:17 +0530

    Update create-multi-arch.sh

--- create-multi-arch.sh
+++ create-multi-arch.sh
@@ -2,11 +2,11 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/rpsene/multi-arch-travis:latest \
-quay.io/rpsene/multi-arch-travis:x86_64 quay.io/rpsene/multi-arch-travis:ppc64le
+docker manifest create quay.io/snehakpersistent/multi-arch-travis:latest \
+quay.io/snehakpersistent/multi-arch-travis:x86_64 quay.io/snehakpersistent/multi-arch-travis:ppc64le
 
-docker manifest inspect quay.io/rpsene/multi-arch-travis
+docker manifest inspect quay.io/snehakpersistent/multi-arch-travis
 
 docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
 
-docker manifest push quay.io/rpsene/multi-arch-travis:latest
+docker manifest push quay.io/snehakpersistent/multi-arch-travis:latest
